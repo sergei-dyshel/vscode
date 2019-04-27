5affb9abf3c1871693039d9c98ca2d129f9e0a62
Sergei Dyshel <qyron.private@gmail.com>
2019-04-27 20:59:58 +0300

    Apply telemetry removal from vscodium.com

--- src/vs/workbench/electron-sandbox/desktop.contribution.ts
+++ src/vs/workbench/electron-sandbox/desktop.contribution.ts
@@ -322,7 +322,7 @@ import { registerWorkbenchContribution2, WorkbenchPhase } from '../common/contri
 			'telemetry.enableCrashReporter': {
 				'type': 'boolean',
 				'description': localize('telemetry.enableCrashReporting', "Enable crash reports to be collected. This helps us improve stability. \nThis option requires restart to take effect."),
-				'default': true,
+				'default': false,
 				'tags': ['usesOnlineServices', 'telemetry'],
 				'markdownDeprecationMessage': localize('enableCrashReporterDeprecated', "If this setting is false, no telemetry will be sent regardless of the new setting's value. Deprecated due to being combined into the {0} setting.", `\`#${TELEMETRY_SETTING_ID}#\``),
 			}
