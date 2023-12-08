42839a8134a6419fc42edabf275013be421f3b55
Sergei Dyshel <qyron.private@gmail.com>
2023-12-08 09:33:17 +0530

    Fixes to my scripts

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ yarn
 yarn postinstall
 
 yarn gulp compile-build
+yarn gulp compile-extension-media
 yarn gulp compile-extensions-build
 yarn gulp minify-vscode
 
 # MacOS-specific
 rm -rf ../VSCode-darwin-x64
-yarn gulp vscode-darwin-x64-min
-yarn gulp vscode-darwin-x64
+yarn gulp vscode-darwin-x64-min-ci
+# yarn gulp vscode-darwin-x64
