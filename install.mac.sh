c820d0fbb0be1c42b5f1bdfa0d6eb71de3baceec
Sergei Dyshel <qyron.private@gmail.com>
2023-12-08 09:33:17 +0530

    Fixes to my scripts

--- install.mac.sh
+++ install.mac.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 name="Code - OSS.app"
-rm -rf "~/temp/$name"
-mv "/Applications/$name" ~/temp/ || true
+rm -rf  "/Applications/$name"
 cp -R "../VSCode-darwin-x64/Visual Studio Code.app" "/Applications/$name"
